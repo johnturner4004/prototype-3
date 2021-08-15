67ecc05966dfb3294d70b0b23d7d33fa32867880
johnturner4004 <johnturner4004@gmail.com>
2021-08-14 21:50:22 -0400

    create spawn manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+  public GameObject obsticalPrefab;
+  private Vector3 spawnPosition = new Vector3(25, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(obsticalPrefab, spawnPosition, obsticalPrefab.transform.rotation);
     }
 
     // Update is called once per frame
