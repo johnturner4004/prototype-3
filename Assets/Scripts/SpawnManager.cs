8ac87a867ccd2856b23e3ddd9c37dce447d48651
johnturner4004 <johnturner4004@gmail.com>
2021-08-16 21:03:15 -0400

    create repeat background script

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
   public GameObject obsticalPrefab;
+  public float startDelay = 2.0f;
+  public float repeatRate = 2.0f;
   private Vector3 spawnPosition = new Vector3(25, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obsticalPrefab, spawnPosition, obsticalPrefab.transform.rotation);
+      InvokeRepeating("SpawnObstical", startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstical()
+    {
+        Instantiate(obsticalPrefab, spawnPosition, obsticalPrefab.transform.rotation);
     }
 }
