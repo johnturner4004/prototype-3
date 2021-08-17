d13e6476e1d9048bc5716336408ecbe1f941508e
johnturner4004 <johnturner4004@gmail.com>
2021-08-16 21:25:24 -0400

    creat function to repeat the background

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+  private Vector3 startPosition;
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
+  {
+    startPosition = transform.position;
+  }
 
-    // Update is called once per frame
-    void Update()
+  // Update is called once per frame
+  void Update()
+  {
+    if (transform.position.x < startPosition.x - 50.0f)
     {
-        
+      transform.position = startPosition;
     }
+  }
 }
