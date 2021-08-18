644b34df92708e19363e34ffd878e8f2015b01ca
johnturner4004 <johnturner4004@gmail.com>
2021-08-18 14:56:11 -0400

    edit move left script to stop on game over

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
   private float speed = 30.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  private PlayerController playerControllerScript;
+  // Start is called before the first frame update
+  void Start()
+  {
+    playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+  }
 
-    // Update is called once per frame
-    void Update()
+  // Update is called once per frame
+  void Update()
+  {
+    if (playerControllerScript.gameOver == false)
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+      transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
+  }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
   public float jumpForce = 10.0f;
   public float gravityModifier = 1.0f;
   private bool isOnGround = true;
-  private bool gameOver = false;
+  public bool gameOver = false;
   // Start is called before the first frame update
   void Start()
   {
