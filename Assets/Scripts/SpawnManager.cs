ddcdc7b478ade7fc958d3bfa19bf8a06ca017fea
johnturner4004 <johnturner4004@gmail.com>
2021-08-18 15:13:30 -0400

    edit spawn manager to stop spawning on game over

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,10 +8,12 @@ public class SpawnManager : MonoBehaviour
   public float startDelay = 2.0f;
   public float repeatRate = 2.0f;
   private Vector3 spawnPosition = new Vector3(25, 0, 0);
+  private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
       InvokeRepeating("SpawnObstical", startDelay, repeatRate);
+      playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -22,6 +24,9 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnObstical()
     {
+      if (playerControllerScript.gameOver == false)
+      {
         Instantiate(obsticalPrefab, spawnPosition, obsticalPrefab.transform.rotation);
+      }
     }
 }
