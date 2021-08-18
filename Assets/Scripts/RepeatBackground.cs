4340bece8121c2b0579cac08392a770962776582
johnturner4004 <johnturner4004@gmail.com>
2021-08-18 12:20:14 -0400

    use box collider to make reset position more precise

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class RepeatBackground : MonoBehaviour
 {
   private Vector3 startPosition;
+  private float repeatPosition;
     // Start is called before the first frame update
     void Start()
   {
     startPosition = transform.position;
+    repeatPosition = GetComponent<BoxCollider>().size.x / 2;
   }
 
   // Update is called once per frame
   void Update()
   {
-    if (transform.position.x < startPosition.x - 50.0f)
+    if (transform.position.x < startPosition.x - repeatPosition)
     {
       transform.position = startPosition;
     }
