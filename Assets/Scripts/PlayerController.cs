04391f06420b055d2d5da8b43ff6773705bfa3a9
johnturner4004 <johnturner4004@gmail.com>
2021-08-19 15:40:27 -0400

    add death animation to game over event

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -39,6 +39,8 @@ public class PlayerController : MonoBehaviour
     {
       gameOver = true;
       Debug.Log("Game Over");
+      playerAnimator.SetBool("Death_b", true);
+      playerAnimator.SetInteger("DeathType_int", 1);
     }
   }
 }
